a106a6f0bd4d7d3c5761e7ba864f4759b361415c
Elio <eliocamison@gmail.com>
2018-02-21 00:31:51 +0100

    test in class main

--- src/com/elioCamison/printer/Main.java
+++ src/com/elioCamison/printer/Main.java
@@ -14,7 +14,8 @@ public class Main {
                         "# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  # ";
 
         Printer p = new Printer(data);
-
+        //--o -----------------------------------------------------------------------------------------------------
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
         int height = 0;
         int width = 0;
         //--o Detectam quants caracter té una línea i quantes línea hi ha. D'aquesta forma podem donar una tamany coherent a l'array
@@ -25,12 +26,42 @@ public class Main {
                 width++;
             }
         }
-        //--o Altura
+        //--o -----------------------------------------------------------------------------------------------------
+
+        /*//--o Altura
         height+=1;
         //--o Anchura
-        width /= 27;
-        char lletra [][] = new char [height][width];
+        width /= 27;*/
 
+        int amp = width / 27;
+        int alt = height+=1;
+        char lletra [][] = new char [alt][amp];
+
+        //--o -----------------------------------------------------------------------------------------------------
+
+        int resta = 2;
+        int posicio =0;
+        //--o Recorrem l'alfabet per saber quants de caràcters s'han de restar a la variable data. D'aquesta forma sabrem quina lletra hem de pintar
+        for (int i = 0; i < alfabet.length(); i++) {
+            if(alfabet.charAt(i) == 'B' && alfabet.charAt(i) != 'A'){
+                posicio = i+1;
+                resta *= posicio;
+            }
+            System.out.print(alfabet.charAt(i));
+        }
+
+
+
+        System.out.println();
+        System.out.println(posicio);
+
+        System.out.println();
+        System.out.println(resta);
+
+        System.out.println();
+        System.out.println(width - resta);
+
+        /*
         //--o Gañanada *1000
         lletra[0][0] = ' ';
         lletra[0][1] = '#';
@@ -52,12 +83,13 @@ public class Main {
         lletra[4][1] = ' ';
         lletra[4][2] = '#';
 
+        //--o Pintar la lletra :D
         for (int i = 0; i < lletra.length; i++) {
             System.out.printf("\n");
             for (int j = 0; j < lletra[i].length; j++) {
                 System.out.printf("%s",lletra[i][j]);
             }
-        }
+        }*/
 
     }
 }
